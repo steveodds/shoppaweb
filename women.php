a0b10a84946c8a08324194ea56152cd8c5e2d83b
Stephen Odundo <steveremdi@gmail.com>
2019-11-26 23:01:01 +0300

    Added new images and changed page structure
    The women's page is now a php file.
    Added product images for the women's page.
    The women's page now connects to the db for product info

--- women.php
+++ women.php
@@ -1,3 +1,8 @@
+<?php
+include 'DBController.php';
+$db_handle = new DBController();
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -6,6 +11,7 @@
     <img src="img\logo.png" alt="LOGO">
   </header>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="styleT.css">
 </head>
 
 <body>
@@ -25,7 +31,7 @@
   <SECTION>
     <h1 class="lol" align="center">NEW WOMEN'S PRODUCTS</h1>
 
-    <p style="float: left; font-size: 15pt; text-align: center; width: 30%; margin-right: 1%; margin-bottom: 0.5em;">
+    <!-- <p style="float: left; font-size: 15pt; text-align: center; width: 30%; margin-right: 1%; margin-bottom: 0.5em;">
       <img src="img\womens1.jpg" style="width: 100%">MARY JANE SHOES//BLACK $350.00</p>
     <p style="float: left; font-size: 15pt; text-align: center; width: 30%; margin-right: 1%; margin-bottom: 0.5em;">
       <img src="img\womens2.jpg" style="width: 100%">TWO PART COURT SHOES//BLACK HEATSEAL $23.00</p>
@@ -39,7 +45,45 @@
         <img src="img\womens5.jpg" style="width: 100%">ASYMMETRIC STRAP BLOCK HEEL SHOES//LEOPARD $500.00</p>
       <p style="float: left; font-size: 15pt; text-align: center; width: 30%; margin-right: 1%; margin-bottom: 0.5em;">
         <img src="img\womens6.jpg" style="width: 100%">TWO PART COURT SHOES//RED $500.00</p>
-      <p style="clear: both;">
+      <p style="clear: both;"> -->
+
+
+      <div id="gridview">
+        <!-- <div class="heading">Product Gallery for Shopping Cart</div> -->
+        <?php
+        $query = $db_handle->runQuery("SELECT * FROM products ORDER BY id ASC");
+        if (!empty($query)) {
+            foreach ($query as $key => $value) {
+                ?>
+                <div class="image">
+                    <img src="<?php echo $query[$key]["image"]; ?>" />
+                    <div class="product-info">
+                        <div class="product-title"><?php echo $query[$key]["name"]; ?></div>
+                        <ul>
+                            <?php
+                                    for ($i = 1; $i <= 5; $i++) {
+                                        $selected = "";
+                                        if (!empty($query[$key]["average_rating"]) && $i <= $query[$key]["average_rating"]) {
+                                            $selected = "selected";
+                                        }
+                                        ?>
+                                <li class='<?php echo $selected; ?>'>★</li>
+                            <?php }  ?>
+                        </ul>
+                        <div class="product-category"><?php echo $query[$key]["category"]; ?>
+
+                        </div>
+                        <div class="add-to-cart">
+                            <div><?php echo $query[$key]["price"]; ?> USD</div>
+                            <div><img src="icon-cart.png" /></div>
+                        </div>
+                    </div>
+                </div>
+        <?php
+            }
+        }
+        ?>
+    </div>
   </SECTION>
 
 </body>
