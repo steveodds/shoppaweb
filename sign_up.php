9db68dfcc9b9b6dab1e0f68796485e86ce5e5235
Stephen Odundo <steveremdi@gmail.com>
2019-12-01 16:25:47 +0300

    Created the sign up page

---
+++ sign_up.php
@@ -0,0 +1,61 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <header>
+        <img src="img\logo.png" alt="LOGO">
+    </header>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="Men.html">Men</a></li>
+        <li><a href="women.php">Women</a></li>
+        <li><a href="Kids.html">Kids</a></li>
+        <li><a href="Brands.html">Brands</a></li>
+        <li><a href="About.html">About</a></li>
+        <li><a href="Contact.html">Contact </a></li>
+    </ul>
+
+
+    <div class="headerForm">
+        <h2>Register</h2>
+    </div>
+
+    <form method="post" action="sign_up.php">
+        <?php include('errors.php'); ?>
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username" value="<?php echo $username; ?>">
+        </div>
+        <div class="input-group">
+            <label>Email</label>
+            <input type="email" name="email" value="<?php echo $email; ?>">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+            <label>Confirm password</label>
+            <input type="password" name="password_2">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn" name="reg_user">Register</button>
+        </div>
+        <p>
+            Already a member? <a href="sign_in.php">Sign in</a>
+        </p>
+    </form>
+
+
+</body>
+<footer>
+    <p>Contact us:+2547890056 About Us</p>
+    <p>All rights reserved ©2019</p>
+</footer>
+
+</html>
