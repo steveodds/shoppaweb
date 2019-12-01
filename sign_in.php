497f782d3e334813bd72402c2a773c2b2820130e
Stephen Odundo <steveremdi@gmail.com>
2019-12-01 16:07:47 +0300

    Added sign in page:
     Also added errors and server php files for db connections

---
+++ sign_in.php
@@ -0,0 +1,54 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <header>
+        <img src="img\logo.png" alt="LOGO">
+    </header>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="Men.html">Men</a></li>
+        <li><a href="women.php">Women</a></li>
+        <li><a href="Kids.html">Kids</a></li>
+        <li><a href="Brands.html">Brands</a></li>
+        <li><a href="About.html">About</a></li>
+        <li><a href="Contact.html">Contact </a></li>
+    </ul>
+
+
+    <div class="headerForm">
+        <h2>Login</h2>
+    </div>
+
+    <form method="post" action="sign_in.php">
+        <?php include('errors.php'); ?>
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn" name="login_user">Login</button>
+        </div>
+        <p>
+            Not yet a member? <a href="sign_up.php">Sign up</a>
+        </p>
+    </form>
+
+
+
+</body>
+<footer>
+    <p>Contact us:+2547890056 About Us</p>
+    <p>All rights reserved ©2019</p>
+</footer>
+
+</html>
