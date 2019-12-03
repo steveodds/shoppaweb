99548a2aa9ea2e22136984a097225bf98ec0041b
Stephen Odundo <steveremdi@gmail.com>
2019-12-03 15:06:19 +0300

    Added new images and wrote the new arrivals pages

---
+++ men/new_arrivals.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <header>
+        <img src="../img/logo.png" alt="LOGO">
+    </header>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+    <link rel="stylesheet" type="text/css" href="../styleT.css">
+</head>
+
+<body>
+    <ul>
+        <li><a href="../index.html">Home</a></li>
+        <li class="dropdown">
+            <a class="active" href="../men.php">Men</a>
+            <div class="dropdown-content">
+                <a class="active" href="../men/new_arrivals.php">New Arrivals</a>
+                <a href="../men/casual.php">Casual</a>
+                <a href="../men/formal.php">Formal</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="../women.php">Women</a>
+            <div class="dropdown-content">
+                <a href="../women/new_arrivals.php">New Arrivals</a>
+                <a href="../women/casual.php">Casual</a>
+                <a href="../women/formal.php">Formal</a>
+            </div>
+        </li>
+        <li class="dropdown">
+            <a href="../kids.php">Kids</a>
+            <div class="dropdown-content">
+                <a href="../kids/boys.php">Boys</a>
+                <a href="../kids/girls.php">Girls</a>
+            </div>
+        </li>
+        <li><a href="../brands.html">Brands</a></li>
+        <li class="dropdown">
+            <a href="../about.html">About</a>
+            <div class="dropdown-content">
+                <a href="../about/locations.html">Store Locations</a>
+                <a href="../about.html">Our Story</a>
+            </div>
+        </li>
+        <li><a href="../contact.html">Contact </a></li>
+        <li class="cart-button">
+            <a href="../cart.php">
+                <img src="../img/icon-cart.png" class="cart-T-image">
+            </a>
+        </li>
+    </ul>
+
+    <SECTION>
+        <h1 class="lol" align="center">MEN'S NEW ARRIVALS</h1>
+        <section>
+            <img src="../img/men_new-1.jpg" class="men-NEW">
+            <img src="../img/men_new-2.jpg" class="men-NEW">
+            <img src="../img/men_new-3.jpg" class="men-NEW">
+        </section>
+    </SECTION>
+
+</body>
+<footer>
+    <p>Contact us:+2547890056 About Us</p>
+    <p>All rights reserved ©2019</p>
+</footer>
+
+</html>
